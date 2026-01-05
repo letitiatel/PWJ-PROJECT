6ef55695d163bec0a6f30ac52f9817faaea8b26f
letitiatel <letitia.telicov@gmail.com>
2026-01-05 22:27:40 +0200

    Created project and database

---
+++ src/test/java/com/cabinetmedical/CabinetMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabinetmedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetMedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
